5d1c22702a4682455712bf6621f682f4438b359c
KosaGabo <gabo.it@protonmail.com>
2023-01-16 17:45:37 +0100

    end of section 11

--- API/Controllers/AccountController.cs
+++ API/Controllers/AccountController.cs
@@ -46,27 +46,29 @@ namespace API.Controllers
         }
 
         [HttpPost("login")]
-         public async Task<ActionResult<UserDto>> Login(LoginDto loginDto)
-         {
-            var user = await _context.Users.SingleOrDefaultAsync(x => x.UserName == loginDto.Username);
+        public async Task<ActionResult<UserDto>> Login(LoginDto loginDto)
+        {
+            var user = await _context.Users
+            .Include(p => p.Photos)
+            .SingleOrDefaultAsync(x => x.UserName == loginDto.Username);
 
-            if(user == null) return Unauthorized("Invalid Username"); 
+            if (user == null) return Unauthorized("Invalid Username");
 
             using var hmac = new HMACSHA512(user.PasswordSalt);
             var computedHash = hmac.ComputeHash(Encoding.UTF8.GetBytes(loginDto.Password));
 
             for (int i = 0; i < computedHash.Length; i++)
             {
-                if(computedHash[i] != user.PasswordHash[i]) return Unauthorized("Invaild Password");
+                if (computedHash[i] != user.PasswordHash[i]) return Unauthorized("Invaild Password");
             }
 
             return new UserDto
             {
                 Username = user.UserName,
                 Token = _tokenService.CreateToken(user),
-
+                PhotoUrl = user.Photos.FirstOrDefault(x => x.IsMain)?.Url
             };
-         }
+        }
 
         private async Task<bool> UserExist(string username)
         {
