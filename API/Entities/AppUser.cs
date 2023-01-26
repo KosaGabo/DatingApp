4aa0b733c0414c9ee43ca6d9b3cd9f40a632a353
KosaGabo <gabo.it@protonmail.com>
2023-01-26 13:36:10 +0100

    end of section 15

--- API/Entities/AppUser.cs
+++ API/Entities/AppUser.cs
@@ -25,5 +25,9 @@ namespace API.Entities
 
         public List<UserLike> LikedUsers { get; set; }
 
+        public List<Message> MessagesSent { get; set; }
+
+        public List<Message> MessagesReceived { get; set; }
+
     }
 }
