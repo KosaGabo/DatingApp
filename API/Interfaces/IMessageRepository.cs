14b5a45c6cb2237f5cae1f72c4b5661f623d03ad
KosaGabo <gabo.it@protonmail.com>
2023-01-31 21:33:45 +0100

    end of section 18

--- API/Interfaces/IMessageRepository.cs
+++ API/Interfaces/IMessageRepository.cs
@@ -1,6 +1,7 @@
 using API.DTOs;
 using API.Entities;
 using API.Helpers;
+using Newtonsoft.Json.Linq;
 
 namespace API.Interfaces
 {
@@ -10,8 +11,7 @@ namespace API.Interfaces
         void DeleteMessage(Message message);
         Task<Message> GetMessage(int id);
         Task<PagedList<MessageDto>> GetMessagesForUser(MessageParams messageParams);
-        Task<IEnumerable<MessageDto>> GetMessageThread(string currentUserName, string recipientUserName);
-        Task<bool> SaveAllAsync();
+        Task<IEnumerable<MessageDto>> GetMessageThread(string currentUserName, string recipientUserName); 
         void AddGroup(Group group);
         void RemoveConnection(Connection connection);
         Task<Connection> GetConnection(string connectionId);
--- client/src/app/nav/nav.component.ts
+++ client/src/app/nav/nav.component.ts
@@ -1,8 +1,6 @@
 import { Component, OnInit } from '@angular/core';
 import { Router } from '@angular/router';
 import { ToastrService } from 'ngx-toastr';
-import { Observable, of } from 'rxjs';
-import { User } from '../_models/user';
 import { AccountService } from '../_services/account.service';
 
 @Component({
@@ -13,7 +11,7 @@ import { AccountService } from '../_services/account.service';
 export class NavComponent implements OnInit {
   model: any = {};
 
-  constructor(public accountService: AccountService, private router: Router, 
+  constructor(public accountService: AccountService, private router: Router,
     private toastr: ToastrService) { }
 
   ngOnInit(): void {
@@ -21,7 +19,10 @@ export class NavComponent implements OnInit {
 
   login() {
     this.accountService.login(this.model).subscribe({
-      next: _ => this.router.navigateByUrl('/members')
+      next: _ => {
+        this.router.navigateByUrl('/members');
+        this.model = {};
+      }
     })
   }
 
